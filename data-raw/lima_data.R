f023899fef6bfbd25d2b998e720bfa58eba7b107
Pascal Crépey <crepey@gmail.com>
2024-03-10 03:06:37 +0100

    extensive update

--- data-raw/lima_data.R
+++ data-raw/lima_data.R
@@ -2,8 +2,31 @@
 library(sf)
 library(tidyverse)
 
-lima_covid <- read_csv(url("https://zenodo.org/record/4915889/files/covid19data.csv?download=1"))
+lima_covid_orig <- read_csv(url("https://zenodo.org/record/4915889/files/covid19data.csv?download=1"))
 
+# on peut enlever "FECHA_CORTE", et "UUID", PROVINCIA, DEPARTAMENTO, METODODX
+# et transforme en facteur les variables SEXO, 
+# et ne garder que les données de la première année
+lima_covid <- lima_covid_orig |>
+  select(-c(FECHA_CORTE, UUID, PROVINCIA, DEPARTAMENTO, METODODX, rango_edad)) |>
+  filter(FECHA_RESULTADO <= "2020-12-31") 
+lima_covid$SEXO <- factor(lima_covid$SEXO, levels = c("MASCULINO", "FEMENINO"), labels = c("Homme", "Femme"))
+lima_covid$DISTRITO <- as.factor(lima_covid$DISTRITO)
+#lima_covid$rango_edad <- as.factor(lima_covid$rango_edad)
+colnames(lima_covid) = c("DISTRICT", "AGE", "SEXE", "DATE_RESULTAT", "lon", "lat")
+# library(data.table)
+# lima_covid_orig_dt = fread("https://zenodo.org/record/4915889/files/covid19data.csv?download=1")
+# 
+# lima_covid = lima_covid_orig_dt[FECHA_RESULTADO <= "2020-12-31",]
+# 
+# lima_covid[, ':='(FECHA_CORTE=NULL, 
+#                    SEXO = factor(SEXO, levels = c("MASCULINO", "FEMENINO"), labels = c("M", "F")), 
+#                    rango_edad = as.factor(rango_edad),
+#                    DISTRITO = as.factor(DISTRITO), 
+#                    METODODX=NULL, 
+#                    DEPARTAMENTO=NULL, 
+#                    PROVINCIA=NULL, 
+#                    UUID=NULL)]
 
 peru <- geodata::gadm("peru", path = ".", level = 3) 
 
